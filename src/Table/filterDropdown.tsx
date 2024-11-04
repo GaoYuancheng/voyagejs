e4179512c38909b4031a8af35c7d3abf69c0e3d3
Hencky <928856778@qq.com>
2024-11-04 20:34:32 +0800

    feat: 类型优化

--- src/Table/filterDropdown.tsx
+++ src/Table/filterDropdown.tsx
@@ -1,56 +1,12 @@
 import { Divider } from 'antd';
 import type { FilterDropdownProps as AFilterDropdownProps } from 'antd/lib/table/interface';
 import React from 'react';
-import { Form } from '../form';
 import { ButtonActions, parsePlugin } from '../plugins';
 import { pluginStore } from '../utils';
 import type { TableStore } from './store';
 
-const { Item: FormItem } = Form;
-
-// filterDropdown: (ctx) => {
-//   const { clearFilters, close, confirm, filters, selectedKeys, setSelectedKeys, visible } = ctx;
-//   return (
-//     <div>
-//       <div style={{ padding: 8 }}>
-//         <Input
-//           placeholder="请输入"
-//           onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-//           value={selectedKeys[0]}
-//         />
-//       </div>
-//       <Divider style={{ margin: 0 }} />
-//       <ButtonActions
-//         style={{ display: 'flex', justifyContent: 'flex-end', padding: 8 }}
-//         actions={[
-//           {
-//             children: '重置',
-//             size: 'small',
-//             type: 'link',
-//             disabled: selectedKeys.length === 0,
-//             onClick: () => {
-//               setSelectedKeys([]);
-//               confirm({ senderName: selectedKeys });
-//               clearFilters();
-//               close();
-//             },
-//           },
-//           {
-//             children: '确定',
-//             size: 'small',
-//             type: 'primary',
-//             onClick: async () => {
-//               confirm({ senderName: selectedKeys });
-//               close();
-//             },
-//           },
-//         ]}
-//       />
-//     </div>
-//   );
-// },
-
-export interface FilterDropdownProps<RecordType> extends React.PropsWithChildren<AFilterDropdownProps> {
+export interface FilterDropdownProps<RecordType extends object = any>
+  extends React.PropsWithChildren<AFilterDropdownProps> {
   table: TableStore<RecordType>;
   dataIndex: string;
   component: string;
@@ -58,7 +14,7 @@ export interface FilterDropdownProps<RecordType> extends React.PropsWithChildren
   ctx?: any;
 }
 
-export const FilterDropdown = <RecordType,>(props: FilterDropdownProps<RecordType>) => {
+export const FilterDropdown = <RecordType extends object = any>(props: FilterDropdownProps<RecordType>) => {
   const {
     clearFilters,
     close,
