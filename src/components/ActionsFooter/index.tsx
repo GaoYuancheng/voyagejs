308b885883953fdac6c71182f7b4c2f0ae349452
Hencky <928856778@qq.com>
2024-11-21 18:04:15 +0800

    feat: update version alpha.13

--- src/components/ActionsFooter/index.tsx
+++ src/components/ActionsFooter/index.tsx
@@ -3,14 +3,15 @@ import { isFunction } from 'radash';
 import React, { useState } from 'react';
 import { QueryActions, QueryActionsProps } from '../../template';
 
-export interface ActionsFooterProps extends QueryActionsProps {
+export interface ActionsFooterProps extends Omit<QueryActionsProps, 'style'> {
   items: QueryActionsProps['items'];
   contentStyle?: React.CSSProperties | ((affixed: boolean) => React.CSSProperties);
   footerStyle?: React.CSSProperties | ((affixed: boolean) => React.CSSProperties);
+  style?: React.CSSProperties | ((affixed: boolean) => React.CSSProperties);
 }
 
 export const ActionsFooter: React.FC<ActionsFooterProps> = (props) => {
-  const { children, contentStyle, footerStyle, ...restProps } = props;
+  const { children, contentStyle, footerStyle, style, ...restProps } = props;
   const [affixed, setAffixed] = useState(false);
 
   return (
@@ -24,10 +25,20 @@ export const ActionsFooter: React.FC<ActionsFooterProps> = (props) => {
         style={{
           background: '#fff',
           boxShadow: affixed ? '0px -6px 13px 0px #ccc' : undefined,
-          ...(isFunction(contentStyle) ? contentStyle(affixed) : contentStyle),
+          ...(isFunction(style) ? style(affixed) : style),
         }}
       >
-        {children && <div style={{ padding: 24, borderBottom: '1px solid #d9d9d9' }}>{children}</div>}
+        {children && (
+          <div
+            style={{
+              padding: 24,
+              borderBottom: '1px solid #d9d9d9',
+              ...(isFunction(contentStyle) ? contentStyle(affixed) : contentStyle),
+            }}
+          >
+            {children}
+          </div>
+        )}
         <Layout.Footer
           style={{ padding: 24, background: '#fff', ...(isFunction(footerStyle) ? footerStyle(affixed) : footerStyle) }}
         >
